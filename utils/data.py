e005663678cb8e98d96dbdb15a7f99f6c3b53138
natoromano <nathanaelpromano@gmail.com>
2016-11-29 18:35:42 -0800

    First training

--- utils/data.py
+++ utils/data.py
@@ -33,6 +33,16 @@ class Datasets(object):
     def __init__(self, **kwargs):
         for argname, dataset in kwargs.iteritems():
             setattr(self, argname, dataset)
+            
+        self._dimension = dataset._patients.shape[1]
+    
+    
+    @property
+    def dimension(self):
+        """
+        Data dimensionality.
+        """
+        return self._dimension
 
 
 
@@ -49,7 +59,9 @@ class Dataset(object):
             usecols=range(704)[:-4]
         )
         self._patients = data[:, :-1]
-        self._labels = data[:, -1]
+        self._labels = np.zeros((data.shape[0], 2))
+        self._labels[data[:, -1]==0, 0] = 1
+        self._labels[data[:, -1]==1, 1] = 1
         self._num_patients = data.shape[0]
         self._index_in_epoch = 0
         self._epochs_completed = 0
@@ -63,14 +75,14 @@ class Dataset(object):
             # Finished epoch
             self._epochs_completed += 1
             # Shuffle the data
-            perm = numpy.arange(self._num_patients)
-            numpy.random.shuffle(perm)
+            perm = np.arange(self._num_patients)
+            np.random.shuffle(perm)
             self._patients = self._patients[perm]
             self._labels = self._labels[perm]
             # Start next epoch
             start = 0
             self._index_in_epoch = batch_size
-            assert batch_size <= self._num_examples
+            assert batch_size <= self._num_patients
 
         end = self._index_in_epoch
         return self._patients[start:end], self._labels[start:end]
